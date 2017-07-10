ccac235b51006db18c18cf47ac922341b560bf28
Jordan Blakey <jordan@jordanblakey.com>
2017-07-09 22:29:05 -0500

    Quick commit.

--- server.js
+++ server.js
@@ -19,13 +19,28 @@ io.sockets.on('connection', function(socket){
 
   // Disconnect
   socket.on('disconnect', function(data){
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length)
-    });
+  });
 
   // Send message
   socket.on('send message', function(data){
     console.log(data);
     io.sockets.emit('new message', {msg:data});
   });
+
+  // New User
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+    console.log(users);
+  }
 });
